69632f1fd793884ca5e43f8505e53e8805c6e3d8
Sentinical <110737516+Drofx@users.noreply.github.com>
2023-07-24 18:18:06 -0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,65 @@
+  const field1 = [
+    { name: "campo-1-view-1.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-1-view-1.jpg" },
+    { name: "campo-1-view-2.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-1-view-2.jpg" },
+    { name: "campo-1-view-3.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-1-view-3.jpg" },
+    { name: "campo-1-view-4.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-1-view-4.jpg" },
+    { name: "campo-1-view-5.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-1-view-5.jpg" }
+  ];
+
+  const field2 = [
+    { name: "campo-2-view-1.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-2-view-1.jpg" },
+    { name: "campo-2-view-2.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-2-view-2.jpg" },
+    { name: "campo-2-view-3.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-2-view-3.jpg" },
+    { name: "campo-2-view-4.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-2-view-4.jpg" },
+    { name: "campo-2-view-5.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-2-view-5.jpg" }
+  ];
+
+  const field3 = [
+    { name: "campo-3-view-1.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-3-view-1.jpg" },
+    { name: "campo-3-view-2.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-3-view-2.jpg" },
+    { name: "campo-3-view-3.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-3-view-3.jpg" },
+    { name: "campo-3-view-4.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-3-view-4.jpg" },
+    { name: "campo-3-view-5.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-3-view-5.jpg" }
+  ];
+
+  const field4 = [
+    { name: "campo-4-view-1.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-4-view-1.jpg" },
+    { name: "campo-4-view-2.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-4-view-2.jpg" },
+    { name: "campo-4-view-3.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-4-view-3.jpg" },
+    { name: "campo-4-view-4.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-4-view-4.jpg" },
+    { name: "campo-4-view-5.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/campo-4-view-5.jpg" }
+  ];
+
+  const openGame = [
+    { name: "jogo-aberto-view-1.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/jogo-aberto-view-1.jpg" },
+    { name: "jogo-aberto-view-2.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/jogo-aberto-view-2.jpg" },
+    { name: "jogo-aberto-view-3.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/jogo-aberto-view-3.jpg" },
+    { name: "jogo-aberto-view-4.jpg", src: "https://paintballstorage.blob.core.windows.net/imagespaintball/jogo-aberto-view-4.jpg" }
+  ];
+
+  export const { thumbImg, equipmentGunImg, equipmentVestImg, equipmentMaskImg } = {
+    thumbImg: {
+      name: "thumb.jpg",
+      src: "https://paintballstorage.blob.core.windows.net/imagespaintball/thumb.jpg"
+    },
+    equipmentGunImg: {
+      name: "equipamento-arma.jpg",
+      src: "https://paintballstorage.blob.core.windows.net/imagespaintball/equipamento-arma-1.jpg"
+    },
+    equipmentMaskImg: {
+      name: "equipamento-mascara.jpg",
+      src: "https://paintballstorage.blob.core.windows.net/imagespaintball/equipamento-mascara-1.jpg"
+    },
+    equipmentVestImg: {
+      name: "equipamento-colete.jpg",
+      src: "https://paintballstorage.blob.core.windows.net/imagespaintball/equipamento-colete-1.jpg"
+    },
+  };
+
+  export {
+    field1,
+    field2,
+    field3,
+    field4,
+    openGame
+  };
